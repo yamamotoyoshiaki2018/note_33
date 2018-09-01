2b2ed1052f40174a7a59e59ba0965085f6d783e9
山本賢明 <3rdpartyjapan@gmail.com>
2018-09-01 21:40:48 +0900

    fix deploy file

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,7 +2,6 @@ require 'carrierwave/storage/abstract'
 require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
-if Rails.env.production?
 CarrierWave.configure do|config|
   config.storage = :fog
   config.fog_provider = 'fog/aws'
@@ -15,5 +14,4 @@ CarrierWave.configure do|config|
 
   config.fog_directory  = 'note33'
   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/note33'
-end
 end
