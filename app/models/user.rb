e3279f303c84f2ff89e6c70552d66d9492a4f82f
山本賢明 <3rdpartyjapan@gmail.com>
2018-09-16 17:25:46 +0900

    user_serch

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  def self.search(search) #self.でクラスメソッドとしている
+    if search # Controllerから渡されたパラメータが!= nilの場合は、titleカラムを部分一致検索
+      User.where(['name LIKE ?', "%#{search}%"])
+    else
+      User.all #全て表示。
+    end
+  end
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
@@ -33,5 +40,4 @@ class User < ApplicationRecord
   def following?(other_user)
     self.followings.include?(other_user)
   end
-
 end
