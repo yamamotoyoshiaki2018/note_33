855cc2d6dc7a68f8fb535e07c02ce366dc1e3152
山本賢明 <3rdpartyjapan@gmail.com>
2018-09-20 10:13:47 +0900

    Note/index

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -3,26 +3,14 @@ class NotesController < ApplicationController
   def index
 
     @like = Like.create(user_id: current_user.id, post_text_id: params[:post_text_id])
-    if params[:tag]
-      @post_texts = PostText.tagged_with(params[:tag]).order("created_at DESC")
-    else
-      @post_texts = PostText.includes(:user).order("created_at DESC")
-    end
+    # if params[:tag]
+    #   @post_texts = PostText.tagged_with(params[:tag]).order("created_at DESC")
+    # else
+    #   @post_texts = PostText.includes(:user).order("created_at DESC")
+    # end
 
-    @post_images = PostImage.includes(:user).order("created_at DESC")
-    # @post_texts = PostText.includes(:user).order("created_at DESC")
-
-
-    # @post_text = PostText.find(params[:id])
-
-    # @post_text = PostText.find_by(id: note.post_text.id)
-    # @post_texts = PostText.includes(:user).order("created_at DESC")
-
-    # @post_text = PostText.includes(:user).order("created_at DESC")
-    # @note = Note.find(params[:id])
     @notes = Note.includes(:user).order("created_at DESC")
-    # @magazines = Magazine.where(magazine_author_id: current_user.id).order("updated_at DESC")
-    # binding.pry
+    @magazines = Magazine.where(magazine_author_id: current_user.id).order("updated_at DESC")
   end
 
   def edit
